0222abb28396cb66e95b124a50c4ce8691aa1fb2
Theocharis Anesiadis <xarhsanesiadhs123@gmail.com>
2025-08-14 17:55:38 +0300

    Prepare for Netlify deployment

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wsztkioepqioqozdsrxg.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzenRraW9lcHFpb3FvemRzcnhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNDM0NDIsImV4cCI6MjA2MjYxOTQ0Mn0.Fn4ZTkeEDsEzLCOVh4YFq_nFV7QALgDPGhDZIcnPfeU";
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
